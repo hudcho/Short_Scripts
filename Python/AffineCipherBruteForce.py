f80e4feec9b07b5560d8d1b28f6f3baf0c9f69b4
hudcho <92966081+hudcho@users.noreply.github.com>
2025-09-02 18:26:35 -0400

    Update AffineCipherBruteForce.py
    Added comments for clarity

--- Python/AffineCipherBruteForce.py
+++ Python/AffineCipherBruteForce.py
@@ -1,14 +1,33 @@
-#Simple script that brute forces an affine cipher shift. This version assumes that the multiplier, A, is known and that ciphertext is also known
-#Iterates through 26 shift possibilites for B allowing you to quickly see which shift provides readable english
+"""
+Program: AffineCipherBruteForce.py
+Author: Hudson Cho
+Date: 09.01.25
+Description: Simple script to brute force an affine cipher when the multiplier
+             (alpha) is known
 
-
+Usage: Enter ciphertext and multiplier (alpha), look in output for readable english text
+Requirements: Python 3.8+ (for modular inverse with pow)
+"""
 
 def decrypt(ciphertext, A, B):
+    """ 
+        Decrypts ciphertext A using the affine decrytpion formula
+
+        Finds modular inverse of A using pow(A, -1, 26), although the extended euclidean algorithm would also work here
+        Applies this to formula A_inv * (x - B) % 26 = plaintext
+
+        Args:
+            ciphertext: encoded ciphertext
+            A: multiplier alpha that was used in the affine encryption alg to encrypt ciphertext
+            B: Shift value B that was used in affine encryption alg to encrypt ciphertext
+        Returns:
+            plaintext: decryped plaintext
+    """
     plaintext = "" 
     for char in ciphertext:
         if char.isalpha():
             x = ord(char.upper()) - ord('A') 
-            A_inv = pow(A, -1, 26) 
+            A_inv = pow(A, -1, 26) #Value erorr thrown if there exists a gcd(A,26) != 1
 
             ptNum = A_inv * (x - B) % 26
             
@@ -19,6 +38,13 @@ def decrypt(ciphertext, A, B):
     return plaintext
 
 def bruteForceIt (ciphertext, A):
+    """
+        Calls upon decrypt 26 times to try every shift value that B can take on (0-25)
+
+        Args:
+            ciphertext: encrpyted ciphertext
+            A: Known multiplier (alpha) that was used in the encryption process
+    """
     for B in range(26):
         plaintext = decrypt(ciphertext, A, B)
         print(f"Shift is {B}: {plaintext}")
@@ -26,4 +52,3 @@ def bruteForceIt (ciphertext, A):
 ciphertext = input("Enter ciphertext: ")
 multiplier = int(input("Enter multiplier: "))
 bruteForceIt(ciphertext, multiplier)
-
