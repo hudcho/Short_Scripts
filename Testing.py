8e5b5a692ad0f8614f44a6d2ccc1fac6ac070213
hudcho <92966081+hudcho@users.noreply.github.com>
2025-09-02 10:53:22 -0400

    Add files via upload

---
+++ Testing.py
@@ -0,0 +1,24 @@
+
+def decrypt(ciphertext, A, B):
+    plaintext = "" 
+    for char in ciphertext:
+        if char.isalpha():
+            x = ord(char.upper()) - ord('A') 
+            A_inv = pow(A, -1, 26) 
+
+            ptNum = A_inv * (x - B) % 26
+            
+            ptChar = chr(ptNum + ord('A'))
+            plaintext += ptChar if char.isupper() else ptChar.lower()
+        else:
+            plaintext += char
+    return plaintext
+
+def bruteForceIt (ciphertext, A):
+    for B in range(26):
+        plaintext = decrypt(ciphertext, A, B)
+        print(f"Shift is {B}: {plaintext}")
+    
+ciphertext = input("Enter ciphertext: ")
+multiplier = int(input("Enter multiplier: "))
+bruteForceIt(ciphertext, multiplier)
